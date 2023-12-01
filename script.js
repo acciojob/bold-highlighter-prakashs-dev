af4382a9659358b22760b9d63d5428b7571b92af
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-12-01 16:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const changeColor = document.querySelectorAll("strong");
 function highlight() {
     //Write your code here
-
+for (let i = 0; i < changeColor.length; i++) {
+	changeColor[i].style.color = "green";
+}
 
 }
 
-
 function return_normal() {
     //Write your code here
-
+for (let i = 0; i < changeColor.length; i++) {
+    changeColor[i].style.color = "black";
+}
     
 }
